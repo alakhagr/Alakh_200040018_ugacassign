083941ba1e69f2693c55f106fb3025898d2a2898
Alakh <alakhagr@gmail.com>
2021-05-09 21:14:18 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Student Info{
+  No.: number;
+  Name: string;
+  Roll No.: number;
+  Department: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebCon';
+
+
+headers = ["No.", "Name", "Roll No.", "Department"];
+
+rows = [
+  
+  {
+    "No."  : "1",
+    "Name" : "A",
+    "Roll No." : "200010001",
+    "Department" : "Aerospace Engineering"
+  },
+
+  {
+    "No."  : "2",
+    "Name" : "B",
+    "Roll No." : "200020001",
+    "Department" : "Biotech Engineering"
+  },
+
+  {
+    "No."  : "3",
+    "Name" : "C",
+    "Roll No." : "200030001",
+    "Department" : "Chemical Engineering"
+  },
+
+  {
+    "No."  : "4",
+    "Name" : "D",
+    "Roll No." : "200040001",
+    "Department" : "Civil Engineering"
+  },
+
+  {
+    "No." : "5",
+    "Name" : "E",
+    "Roll No." : "200050001",
+    "Department" : "Computer Science Engineering"
+  },
+
+];
+
 }
